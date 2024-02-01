c0198913b307f4969e17de5dd5448775885579da
jed0203 <cha19057@byui.edu>
2024-02-01 00:38:15 -0700

    edit bookresult file

--- pages/components/BookResult.js
+++ pages/components/BookResult.js
@@ -6,19 +6,20 @@
  */
 
 const BookResult = ({ book }) => {
-   // Destructure book object
-  const { title, author_name, first_publish_year, cover_i } = book;
+  if (!book) {
+    return null; // Render nothing if book is undefined or null
+  }
+
+  const { title = '', author_name = [], first_publish_year = '', cover_i } = book;
 
   return (
     <div className="book-result">
       <div className="book-cover">
-        {/* Render book cover */}
-        <img src={`https://covers.openlibrary.org/b/id/${cover_i}-L.jpg`} alt={title} />
+        {cover_i && <img src={`https://covers.openlibrary.org/b/id/${cover_i}-L.jpg`} alt={title} />}
       </div>
-      {/* Render book details */}
       <div className="book-details">
         <h3>{title}</h3>
-        <p>Author: {author_name}</p>
+        <p>Author: {author_name.join(', ')}</p>
         <p>Year: {first_publish_year}</p>
       </div>
     </div>
